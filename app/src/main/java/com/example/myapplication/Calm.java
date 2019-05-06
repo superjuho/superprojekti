56495026c81c20042886f2536f9e419bb606695c
superjuho <47350364+superjuho@users.noreply.github.com>
2019-05-06 18:40:00 +0300

    Start nappulan toiminta, nappulavalikko ja Calenterin aloitus.

--- app/src/main/java/com/example/myapplication/Calm.java
+++ app/src/main/java/com/example/myapplication/Calm.java
@@ -3,12 +3,29 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Calm extends AppCompatActivity {
 
+    Button great;
+    Button fine;
+    Button ngreat;
+    Button bad;
+    Button awful;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calm);
+        great = findViewById(R.id.great);
+        fine = findViewById(R.id.Fine);
+        ngreat = findViewById(R.id.ngreat);
+        bad = findViewById(R.id.bad);
+        awful = findViewById(R.id.awful);
+
+    }
+
+    public void greatClick(View v) {
+
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,15 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-private String TAG = "Tässä";
-private int Lol = 0;
+
+
 Button start;
 
     @Override
@@ -20,8 +21,6 @@ Button start;
     }
 
     public void onButtonClick(View v) {
-        Lol++;
-        Log.d(TAG,"Täällä ollaan");
-        Log.d(TAG,"Vittu mitä pasoaa");
+        startActivity(new Intent(MainActivity.this, Calm.class));
     }
 }
