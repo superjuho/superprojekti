e31cbb44eae90e6be3c608b8d34b7ca9334b5b03
wazzazzaa <tomi.eranne@gmail.com>
2019-05-07 18:25:27 +0300

    test

--- app/src/main/java/com/example/myapplication/Relax.java
+++ app/src/main/java/com/example/myapplication/Relax.java
@@ -1,15 +1,12 @@
 package com.example.myapplication;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class Relax extends Activity {
+
+
+public class Relax extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,32 +14,18 @@ public class Relax extends Activity {
         setContentView(R.layout.activity_main);
 
         populateListView();
-        registerClickCallback();
     }
 
 
     private void populateListView() {
         String[] myItems = {"Relax1", "Relax2", "Relax3", "Relax4"};
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+        ArrayAdapter<String> adapter = new ArrayAdapter(
                 this,
                 R.layout.relaxlist,
                 myItems);
 
         ListView list = findViewById(R.id.relaxlist);
         list.setAdapter(adapter);
-    }
-
-    private void registerClickCallback() {
-        ListView list = findViewById(R.id.relaxlist);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> paret, View viewClicked, int position, long id) {
-                TextView textView = (TextView) viewClicked;
-                String message = "You clicked # " + position
-                        + ", which is string: " + textView.getText().toString();
-                Toast.makeText(Relax.this, message, Toast.LENGTH_LONG).show();
-            }
-        });
     }
 }
