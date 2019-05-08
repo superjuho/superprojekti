5a6b347f0a8a2e127df881991a97d3a84f4219fa
Vilja Aspegren <aspegren.vilja@gmail.com>
2019-05-09 00:27:49 +0300

    kaikki napit ja taustavärit

--- app/src/main/java/com/example/myapplication/Breath.java
+++ app/src/main/java/com/example/myapplication/Breath.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Breath luokassa on rauhoittava hengitysharjoitus
@@ -14,5 +16,14 @@ public class Breath extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breath);
+    }
+
+    public void calendarHomeButtonClick(View v) {
+        Calendar.calendarHomeButtonPressed();
+        startActivity(new Intent(Breath.this, Calendar.class));
+    }
+
+    public void relaxHomeButtonClick (View v) {
+        startActivity(new Intent(Breath.this, Relax.class));
     }
 }
